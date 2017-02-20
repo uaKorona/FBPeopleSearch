d501880edf52bd94932390b70a38d650dc99d58d
roman kovalskiy <roman.kovalskyi@trinetix.com>
2017-02-20 11:59:56 +0200

    onTap

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,12 +7,26 @@ import styles from './App.css';
 
 injectTapEventPlugin();
 
-const App = () => (
-  <MuiThemeProvider>
-    <div className={styles.app}>
-      <AppBar title="FBPeopleSearch" />
-    </div>
-  </MuiThemeProvider>
-);
+class App extends React.Component {
+
+  /* eslint-disable class-methods-use-this*/
+
+  onSettingsIconTouchTap(event) {
+    /* eslint-disable no-console */
+    console.log('onSettingsIconTouchTap', event);
+    /* eslint-enable no-console */
+  }
+  /* eslint-enable class-methods-use-this*/
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div className={styles.app}>
+          <AppBar title="FBPeopleSearch" onLeftIconButtonTouchTap={this.onSettingsIconTouchTap} />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
 
 export default App;
