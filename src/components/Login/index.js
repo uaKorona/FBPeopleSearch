96a7e21dfecbd47d3e9d88d790db728607bb4fa8
roman kovalskiy <roman.kovalskyi@trinetix.com>
2017-03-13 12:17:08 +0200

    fail with material's button

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import { amber500, blue900 } from 'material-ui/styles/colors';
+import styles from './login.css';
 
 export default class Login extends React.Component {
-  title = 'Login';
+  title = 'FBPeopleSearch';
+
+  styles = {
+    title: {
+      color: blue900,
+    },
+  };
 
   render() {
     return (
-      <h1>{this.title}</h1>
+      <div className={styles.loginContainer}>
+        <h1 style={this.styles.title}>{this.title}</h1>
+        <RaisedButton className={styles.loginButton} label="LOGIN" backgroundColor={amber500} />
+      </div>
     );
   }
 }
