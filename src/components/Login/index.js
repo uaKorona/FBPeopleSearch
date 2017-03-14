8a36ba1b182a8120ccf25abe0c9b93b75f382cef
roman kovalskiy <roman.kovalskyi@trinetix.com>
2017-03-14 12:21:09 +0200

    flex container

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -10,13 +10,39 @@ export default class Login extends React.Component {
     title: {
       color: blue900,
     },
+    buttonLabel: {
+      fontSize: '1em',
+      color: 'white',
+    },
+    overlayStyle: {
+
+    },
+    button: {
+      padding: '0.3em 2em',
+      height: 'auto',
+    },
   };
+
+  /*
+   display: flex;
+   min-width: 88px;
+   flex-direction: column;
+   justify-content: center;
+   flex-wrap: wrap;
+   height: 100vh;
+   */
 
   render() {
     return (
       <div className={styles.loginContainer}>
         <h1 style={this.styles.title}>{this.title}</h1>
-        <RaisedButton className={styles.loginButton} label="LOGIN" backgroundColor={amber500} />
+        <RaisedButton
+          label="LOGIN"
+          labelStyle={this.styles.buttonLabel}
+          buttonStyle={this.styles.button}
+          overlayStyle={this.styles.button}
+          backgroundColor={amber500}
+        />
       </div>
     );
   }
